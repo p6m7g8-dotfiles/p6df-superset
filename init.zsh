520e45003fcdf71d8ff27679515570b64d6dae34
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-03-28 20:01:54 -0400

    chore: refactor

--- init.zsh
+++ init.zsh
@@ -28,47 +28,31 @@ p6df::modules::superset::mcp() {
 
   p6_js_npm_global_install "superset-mcp"
 
+  p6df::modules::anthropic::mcp::server::add "superset" "npx" "-y" "superset-mcp"
+  p6df::modules::openai::mcp::server::add "superset" "npx" "-y" "superset-mcp"
+
   p6_return_void
 }
 
 ######################################################################
 #<
 #
-# Function: p6df::modules::superset::profile::on(profile, [env_or_url=http://localhost:8088], [username=admin], [password=])
+# Function: p6df::modules::superset::profile::on(profile, code)
 #
 #  Args:
 #	profile -
-#	OPTIONAL env_or_url - [http://localhost:8088]
-#	OPTIONAL username - [admin]
-#	OPTIONAL password - []
+#	code - shell code block (export SUPERSET_URL=... SUPERSET_USERNAME=... SUPERSET_PASSWORD=...)
 #
 #  Environment:	 P6_DFZ_PROFILE_SUPERSET SUPERSET_PASSWORD SUPERSET_URL SUPERSET_USERNAME
 #>
 ######################################################################
 p6df::modules::superset::profile::on() {
   local profile="$1"
-  local env_or_url="${2:-http://localhost:8088}"
-  local username="${3:-admin}"
-  local password="${4:-}"
+  local code="$2"
 
-  local url="$env_or_url"
-
-  if p6_string_match_regex "$env_or_url" '(^|[[:space:]])export[[:space:]]+SUPERSET'; then
-    p6_run_code "$env_or_url"
-    url="${SUPERSET_URL:-http://localhost:8088}"
-    username="${SUPERSET_USERNAME:-$username}"
-    password="${SUPERSET_PASSWORD:-$password}"
-  fi
+  p6_run_code "$code"
 
   p6_env_export "P6_DFZ_PROFILE_SUPERSET" "$profile"
-  p6_env_export "SUPERSET_URL"            "$url"
-  p6_env_export "SUPERSET_USERNAME"       "$username"
-
-  if p6_string_blank_NOT "$password"; then
-    p6_env_export "SUPERSET_PASSWORD" "$password"
-  else
-    p6_env_export_un SUPERSET_PASSWORD
-  fi
 
   p6_return_void
 }
@@ -76,17 +60,19 @@ p6df::modules::superset::profile::on() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::superset::profile::off()
+# Function: p6df::modules::superset::profile::off(code)
+#
+#  Args:
+#	code - shell code block previously passed to profile::on
 #
 #  Environment:	 P6_DFZ_PROFILE_SUPERSET SUPERSET_PASSWORD SUPERSET_URL SUPERSET_USERNAME
 #>
 ######################################################################
 p6df::modules::superset::profile::off() {
+  local code="$1"
 
+  p6_env_unset_from_code "$code"
   p6_env_export_un P6_DFZ_PROFILE_SUPERSET
-  p6_env_export_un SUPERSET_URL
-  p6_env_export_un SUPERSET_USERNAME
-  p6_env_export_un SUPERSET_PASSWORD
 
   p6_return_void
 }
